b2b16844b1a96ac8361465e9ba7855de781a854e
Karim Elawaad <karim.elawaad@hotmail.com>
2017-11-11 23:20:31 +0200

    added email sending to restore the account

--- forgotUsername.php
+++ forgotUsername.php
@@ -1,7 +1,28 @@
 <?php
+
+	$servername = "localhost";
+	$db_username   = "root";
+	$db_password   = "";
+	$dbname     = "registrationsystem";
+
+	$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
 	$email = $_POST["email"];
-	$subject = "Alexandria University | Account Restore";
-	mail($email, $subject, "Hello User,\nPlease follow this link in order to restore your account: https://google.com.\n\nBest Regards,\nAlexandria University IT support.");
-	echo 'Hello';
+	$query = "SELECT * FROM user WHERE email = '$email'";
+	$result = mysqli_query($conn, $query);
+	$username = null;
+	if(mysqli_num_rows($result) > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$username = $row["username"];
+	}
+
+	if($username != null) {
+		$subject = "Alexandria University | Account Restore";
+		mail($email, $subject, "Hello User,\nThis is the username associated with this email: ".$username."\n\nBest Regards,\nAlexandria University IT support.");
+	}
 
 ?>
